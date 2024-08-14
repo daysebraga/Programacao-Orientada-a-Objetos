9d4d74ec9117a05a040efbd6dbb539b3965ec6fe
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-14 20:53:18 -0300

    Create simuladorTorneio.java

---
+++ simuladorTorneio.java
@@ -0,0 +1,15 @@
+public class simuladorTorneio{
+    public static void main(String[] args){
+        public static void menu(Torneio t){
+            
+        }
+        
+        public void gravarTorneioArquivo(){
+            
+        }
+        
+        public void lerTorneioArquivo(){
+            
+        }
+    }
+}
