ac8ea9906ecff574af7b11d4fde8976e874ab55a
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-14 20:52:03 -0300

    Create Dado.java

---
+++ Dado.java
@@ -0,0 +1,15 @@
+public class Dado{
+    private int sideUp;
+    
+    public void roll(){
+        
+    }
+    
+    public int getSideUp(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
