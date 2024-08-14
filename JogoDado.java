5eee56000cfd452770d5d7a6a8e05dc176114051
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-14 20:52:17 -0300

    Create JogoDado.java

---
+++ JogoDado.java
@@ -0,0 +1,16 @@
+public class JogoDados{
+    private Dado[] dados;
+    private int qntDados = 2;
+    
+    public rolarDados(){
+        
+    }
+    
+    public aplicarRegradoJogo(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
