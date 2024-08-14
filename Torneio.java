4f382219d6ea59367f1f9d3b218ce6634686717c
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-14 20:52:51 -0300

    Create Torneio.java

---
+++ Torneio.java
@@ -0,0 +1,25 @@
+public class Torneio{
+    private Jogador[] jogadores;
+    private int qntdeJogadores;
+    private int qntdeRodadas;
+    
+    public void incluirJogador(){
+        
+    }
+    
+    public void removerJogador(){
+        
+    }
+    
+    public void iniciarTorneio(){
+        
+    }
+    
+    public void mostrarPlacarFinal(){
+        
+    }
+    
+    public void mostrarFinaldaRodada(){
+        
+    }   
+}
