c5dfb4b4b92cbdfd1e42e6507bea8d74af45b57c
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-14 21:10:09 -0300

    Update Dado.java

--- Dado.java
+++ Dado.java
@@ -1,15 +1,22 @@
+import java.util.Random;
+
 public class Dado{
     private int sideUp;
-    
-    public void roll(){
-        
+
+    public Dado(){ //construtor padrão, sempre que o objeto dado for criado o lado de cima do dado será 1
+        sideUp = 1;
     }
     
-    public int getSideUp(){
-        
+    public void roll(){ //Gera um número aleatorio entre 1 e 6
+        Random r = new Random();
+        sideUp = r.nextInt(6) + 1;
     }
     
-    public String toString(){
-        
+    public int getSideUp(){ //retorna o lado de cima do dado
+        return sideUp;
+    }
+    
+    public String toString(){ //imprime o lado de cima do dado
+        return "Dado: " + getSideUp();
     }
 }
