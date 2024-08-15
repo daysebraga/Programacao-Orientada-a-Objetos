62a5a1a885a496935cae70ffab346cc4eb71f536
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-15 14:35:44 -0300

    Update Torneio.java

--- Torneio.java
+++ Torneio.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Torneio{
     private Jogador[] jogadores;
-    private int qntdeJogadores;
-    private int qntdeRodadas;
+    private int qntDeJogadores;
+    private int qntDeRodadas;
+    private int mesa = 0; //variavel para somar as apostas da mesa(facilitar a divisao em caso de empate)
+
+    public void apostaDaRodada(){
+        for(int i = 0; i < qntDeJogadores; i++){
+            Scanner valorAposta = new Scanner(System.in);
+            mesa += jogadores[i].apostar(valorAposta); 
+        }
+    }
     
     public void incluirJogador(){
         
