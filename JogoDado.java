1cec25d95cae53a5cd6744d35bf6fe9ce9cb4e7b
Keila Dario <105608713+eoqueila@users.noreply.github.com>
2024-08-15 15:02:53 -0300

    Update JogoDado.java

--- JogoDado.java
+++ JogoDado.java
@@ -1,16 +1,24 @@
 public class JogoDados{    //classe para jogar os dados//
     private Dado[] dados;
     private final int qntDados = 2;
+
+    public JogoDados() {
+        dados = new Dado[qntDados];
+        for (int i = 0; i < qntDados; i++) {
+            dados[i] = new Dado(); // Assumindo que Dado tem um construtor padrão
+   }
+    
     
     public rolarDados(){
-        
+        for (Dado dado : dados) {
+            dado.roll();
+        }
     }
     
     public aplicarRegradoJogo(){
-        
+        //esperar a aplicação dos jogos
     }
     
     public String toString(){
-        
-    }
+     return "Dado 1: " + dados[0].sideUp + " Dado 2: " + dados[1].sideUp;
 }
