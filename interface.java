e3ae3d6c93bc01b56e20962b4fad82d4f94c0c68
Dayse Braga <daysebraga2803@gmail.com>
2024-08-15 14:36:00 -0300

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,6 +1,42 @@
-//interface apresentada ao usuario//
+//interface apresentada ao usuario, simulador do torneio//
 
- public class Interface{
+ public class interface{
+ import java.util.Scanner;
 
-     System.out.print();
+     System.out.println("(1)incluir_jogador\n"+
+                       "(2)remover_jogador\n"+
+                       "(3)inicializar_torneio\n"+
+                       "(4)placar_do_torneio\n"+
+                       "(5)gravar_dados_do_torneio_em_arquivo\n"+
+                       "(6)ler_os_dados_do_torneio_em_arquivo\n"+
+                       "(7)sair_da_aplicacao\n");
+
+  Scanner opcao = new Scanner(System.in);
+
+      switch(opcao){
+
+         case 1:
+            jogador();
+         case 2:
+              System.out.println("insira a indentificacao do jogagor");
+
+              Scanner identificacao = new Scanner(System.in);
+              
+         case 3:
+              if(jogardor > 2)
+
+               else
+                  System.out.print("quantidade jogadores insuficientes");
+        
+         case 4:
+
+         case 5:
+
+         case 6:
+
+         case 7:
+              break;
+           
+     }
+       
  }
