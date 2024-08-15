c7221bf95385efce0eb4ac0fe49bff32915ccc3c
Dayse Braga <daysebraga2803@gmail.com>
2024-08-15 17:02:02 -0300

    Update Torneio.java

--- Torneio.java
+++ Torneio.java
@@ -18,16 +18,31 @@ public class Torneio{
         qntDeJogadores++;
     }
     
-    public void removerJogador( string id){
-    
+    public void removerJogador( string iden){
+            for(int i=0; i<qntDeJogadores; i++){
+                    if(jogadores[i].id.equals(iden)){
+                            jogadores[i].remover_tudo();
+                            qntDeJogadores--;
+                    }
+            }
     }
     
     public void iniciarTorneio(){
 
         if(qntDeJogadores >= 2){
+            do{
+                System.out.println("(1) jogo de azar ou (2) jogo do porquinho: ");
+                Scanner jogo_escolhido = new Scanner(System.in);
+            }while(jogo_escolhido != 1 || jogo_escolhido !=2);
+            
                 do{
-                    
-                }while(!ganhador==1); //fazer o return de 1 para quando for encontrado ganhador(es), criterio de parada//
+                    if(jogo_escolhido == 1){
+                        
+                    }
+                    else if(jogo_escolhido == 2){
+                        
+                    }
+                }while(!ganhador==true); //fazer o return de 1 para quando for encontrado ganhador(es), criterio de parada//
         }else
             System.out.println("quantidade de jogadores insuficiente");     
     }
