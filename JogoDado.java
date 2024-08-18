0ce2278a16d0c73a1643b290c6a1574d064b3595
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-18 15:06:03 -0300

    Update JogoDado.java

--- JogoDado.java
+++ JogoDado.java
@@ -25,8 +25,52 @@ public class JogoDados{    //classe para jogar os dados//
     public String toString(){
         return "Dado 1: " + this.dados[0].getSideUp() + " Dado 2: " + this.dados[1].getSideUp();
     }
+
+    public boolean jogarJogoAzar(Jogador jogador){
+        int[] resultado = getResultado();
+        int soma = resultado[0] + resultado[1];
+
+        if(soma == 7 || soma == 11){
+            System.out.println("Lancamento 1: /n" + resultado[0] + " e " + resultado[1] + " = " + soma);
+            System.out.println("Jogador ganhou.");
+            jogador.setGanhador(true);
+            return true;
+        }else if(soma == 2 || soma == 3 || soma == 12){
+            System.out.println("Lancamento 1: /n" + resultado[0] + " e " + resultado[1] + " = " + soma);
+            System.out.println("Jogador perdeu.");
+            jogador.setGanhador(false);
+            return false;
+        }else{
+            int i = 1;
+            System.out.println("Lancamento " + i + ": /n" + resultado[0] + " e " + resultado[1] + " = " + soma);
+            System.out.println("Número a ser buscado: " + soma);
+            int numeroBuscado = soma;
+            
+            while(true){
+                rolarDados();
+                resultado = getResultado();
+                int novaSoma = resultado[0] + resultado[1];
+                i++;
+                
+                if(novaSoma == numeroBuscado){
+                    System.out.println("Lancamento " + i + ": /n" + resultado[0] + " e " + resultado[1] + " = " + soma);
+                    System.out.println("Jogador ganhou.");
+                    jogador.setGanhador(true);
+                    return true;
+                }else if(novaSoma == 7){
+                    System.out.println("Lancamento " + i + ": /n" + resultado[0] + " e " + resultado[1] + " = " + soma);
+                    System.out.println("Jogador perdeu.");
+                    jogador.setGanhador(false);
+                    return false;
+                }
+            }
+        }
+    }
     
-    public aplicarRegradoJogo(){
-        
+    public aplicarRegradoJogo(int qualJogo, Jogador jogador){
+        if(qualJogo == 1)
+            jogarJogoAzar(jogador);
+        else if(qualJogo == 2)
+            //porquinho
     }
 }
