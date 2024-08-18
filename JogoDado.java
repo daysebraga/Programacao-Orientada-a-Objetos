aece54511dba455a3bec42fcbd03b45efb825512
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-18 14:03:27 -0300

    Update JogoDado.java

--- JogoDado.java
+++ JogoDado.java
@@ -2,23 +2,31 @@ public class JogoDados{    //classe para jogar os dados//
     private Dado[] dados;
     private final int qntDados = 2;
 
-    public JogoDados() {
+    public JogoDados(){
         dados = new Dado[qntDados];
-        for (int i = 0; i < qntDados; i++) {
-            dados[i] = new Dado(); // Assumindo que Dado tem um construtor padrão
+        for(int i = 0; i < qntDados; i++)
+            dados[i] = new Dado();
    }
     
     
     public rolarDados(){
-        for (Dado dado : dados) {
-            dado.roll();
-        }
+        for(int i = 0; i < qntDados; i++)
+            dados[i].roll();
+    }
+
+    public int[] getResultado(){
+        int[] resultado = new int[qntDados];
+        for(int i = 0; i < qntDados; i++)
+            resultado[i] = dados[i].getSideUp();
+        
+        return resultado;
+    }
+
+    public String toString(){
+        return "Dado 1: " + this.dados[0].getSideUp() + " Dado 2: " + this.dados[1].getSideUp();
     }
     
     public aplicarRegradoJogo(){
-        //esperar a aplicação dos jogos
+        
     }
-    
-    public String toString(){
-     return "Dado 1: " + dados[0].sideUp + " Dado 2: " + dados[1].sideUp;
 }
