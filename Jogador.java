4f0046f51aca823a2bc10435ebea8205aba5cec7
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-18 13:14:33 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,10 +1,16 @@
 public class Jogador{
     private string id;
-    private Boolean tipo;
+    private boolean tipo; //true (1) para humano, false (0) para máquina
     private double aposta;
     private JogoDado jogoD = new JogoDado;
     private double saldo = 100;
-    private Boolean ganhador;
+    private boolean ganhador;
+
+    public Jogador(String id, boolean tipo){
+        this.id = id;
+        this.tipo = tipo;
+        this.jogoDado = new JogoDados();
+    }
     
     public void jogarDados(){
         jogoD.rolarDados();
@@ -14,6 +20,7 @@ public class Jogador{
         if(tipo == 0){ //maquina
             int auxiliar = (this.saldo/5);
             this.saldo -= auxiliar;
+            this.aposta = auxiliar;
             return auxiliar;
         }   
     }
@@ -22,6 +29,7 @@ public class Jogador{
         if(tipo == 1){ //humano
             if((saldo > 0) && (qntDaAposta < saldo)){
                 this.saldo -= qntDaAposta;
+                this.aposta = qntDaAposta;
                 return qntDaAposta;
             }
         }
@@ -29,26 +37,42 @@ public class Jogador{
             if(saldo > 0){
                 qntDaAposta = (this.saldo/5);
                 this.saldo -= qntDaAposta;
+                this.aposta = qntDaAposta;
                 return qntDaAposta;
             }
         }
     }
     
-    public void mostrarLancamentosJogo(){
-        jogoD.toString();
+    public int[] mostrarLancamentosJogo(){
+        return jogoD.getResultados();
+    }
+
+    public void ganhou(double valor){
+        this.saldo += valor;
     }
     
     public double getSaldo(){
         return this.saldo;
     }
-    
-    public String toString(){
-        return "Id: " + this.id + ", saldo: " + this.saldo;
+
+    public double getAposta(){
+        return this.aposta;
     }
 
-    public void remover_tudo(){
-        this.id = null;
-        this.saldo = 0;
+    public String getId(){
+        return this.id;
+    }
+
+    public void setGanhador(boolean ganhador){
+        this.ganhador = ganhador;
+    }
+
+    public boolean isGanhador(){
+        return this.ganhador;
     }
     
+    public String toString(){
+        return "Jogador: \n" + "Id: " + this.id + "\n" + "Tipo: " + (this.tipo ? "humano" : "maquina") + "\n" +"Saldo: " + this.saldo + "\n" + "Aposta: " + this.aposta; 
+        //o ? : funciona como if else para verificar se o tipo é humano ou maquina, ja que a variavel é booleana funciona so como um true ou false
+    }
 }
