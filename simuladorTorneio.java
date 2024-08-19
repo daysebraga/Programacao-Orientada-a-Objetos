2578828099893019fe391eed2dfc6a6e4e958201
Dayse Braga <daysebraga2803@gmail.com>
2024-08-19 14:23:08 -0300

    Update simuladorTorneio.java

--- simuladorTorneio.java
+++ simuladorTorneio.java
@@ -1,6 +1,7 @@
 public class simuladorTorneio{
     public static void main(String[] args){
         public static void menu(Torneio t){
+            Menuinterface();
             
         }
         
