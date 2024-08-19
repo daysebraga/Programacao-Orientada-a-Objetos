91580e4ed64144f5d553598546bd7881b583b9aa
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-19 18:06:32 -0300

    Update simuladorTorneio.java

--- simuladorTorneio.java
+++ simuladorTorneio.java
@@ -1,16 +1,5 @@
 public class simuladorTorneio{
     public static void main(String[] args){
-        public static void menu(Torneio t){
-            Menuinterface();
-            
-        }
         
-        public void gravarTorneioArquivo(){
-            
-        }
-        
-        public void lerTorneioArquivo(){
-            
-        }
     }
 }
