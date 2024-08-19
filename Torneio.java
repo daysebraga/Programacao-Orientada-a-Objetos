55887b18ab8427703d6f62f88025c0fc9dccc32a
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-19 18:30:45 -0300

    Update Torneio.java

--- Torneio.java
+++ Torneio.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class Torneio{
     private Jogador[] jogadores;
@@ -11,52 +17,6 @@ public class Torneio{
         jogadores = new Jogador[10];
         qntDeJogadores = 0;
         qntDeRodadas = 0;
-    }
-    
-    private void menuInterface(){
-        System.out.print("(1)incluir_jogador\n"+
-                         "(2)remover_jogador\n"+
-                         "(3)inicializar_torneio\n"+
-                         "(4)placar_do_torneio\n"+
-                         "(5)gravar_dados_do_torneio_em_arquivo\n"+
-                         "(6)ler_os_dados_do_torneio_em_arquivo\n"+
-                         "(7)sair_da_aplicacao\n");
-        
-        int opcao;
-        do{
-            opcao = scanner.nextInt();
-            scanner.nextLine();
-            if((opcao < 1) || (opcao > 7))
-                System.out.println("Opcao invalida. Insira um numero entre 1 e 7.");
-        while((opcao < 1) || (opcao > 7));
-        
-        switch(opcao){
-            case 1:
-                incluirJogador();
-                break;
-            case 2:
-                System.out.println("informe a indentificacao do jogador");
-                String identificacao = scanner.nextLine();
-                removerJogador(identificacao);
-                break;
-            case 3:
-                iniciarTorneio();
-                break;
-            case 4:
-                mostrarPlacarFinal();
-                break;
-            case 5:
-                gravarTorneioArquivo();
-                break;
-            case 6:
-                lerTorneioArquivo();
-                break;
-            case 7:
-                Runtime.getRuntime().exec("cls"); 
-                break;
-            default:
-                System.out.println("Opcao invalida.");
-         }
     }
     
     private void incluirJogador(){
@@ -113,13 +73,11 @@ public class Torneio{
     }
 
     public void apostaDaRodada(){
-        Scanner valorAposta = new Scanner(System.in);
-    
         for(int i = 0; i < qntDeJogadores; i++){
             if(jogadores[i].getSaldo() > 1){
                 if(jogadores[i].isHumano()){
                     System.out.print("Jogador " + jogadores[i].getId() + ", insira o valor da aposta: ");
-                    int aposta = valorAposta.nextInt();
+                    int aposta = scanner.nextInt();
                     mesa += jogadores[i].apostar(aposta);
                 }else
                     mesa += jogadores[i].apostar();
@@ -138,6 +96,7 @@ public class Torneio{
                 do{
                     System.out.println("Escolha o tipo de jogo: (1) Jogo de Azar ou (2) Jogo do Porquinho");
                     jogoEscolhido = scanner.nextInt();
+                    scanner.nextLine();
                     
                     if((jogoEscolhido != 1) && (jogoEscolhido != 2))
                         System.out.println("Opcao invalida.");
@@ -146,6 +105,7 @@ public class Torneio{
                 System.out.print("Informe o número máximo de rodadas: ");
                 int rodada = scanner.nextInt();
                 setQntDeRodadas(rodada);
+                scanner.nextLine();
     
                 boolean ganhadorEncontrado = false;
     
@@ -214,5 +174,95 @@ public class Torneio{
             for(int i = 0; i < qntDeJogadores; i++)
                 System.out.println((i + 1) + " lugar: Jogador " + jogadores[i].getId() + " com saldo de " + jogadores[i].getSaldo());
         }
+    }
+
+    public void gravarTorneioArquivo(){
+        try(PrintWriter escrever = new PrintWriter(new FileWriter("torneio.txt"))){ //cria o objeto PrintWriter e escreve no arquivo
+            escrever.println(qntDeJogadores);
+            escrever.println(qntDeRodadas);
+            
+            for(int i = 0; i < qntDeJogadores; i++){
+                Jogador jogador = jogadores[i];
+                escrever.println(jogador.getId());
+                escrever.println(jogador.isHumano());
+                escrever.println(jogador.getSaldo());
+                escrever.println(jogador.getAposta());
+                escrever.println(jogador.isGanhador());
+            }
+            
+        }catch(IOException e){ 
+            System.out.println("Erro ao gravar os dados do torneio: " + e.getMessage()); //pra imprimir o nome do erro em caso de excecao
+        }
+    }
+
+    public void lerTorneioArquivo(){
+        try(BufferedReader ler = new BufferedReader(new FileReader("torneio.txt"))){ //cria o objeto para ler o arquivo
+            qntDeJogadores = Integer.parseInt(ler.readLine());
+            qntDeRodadas = Integer.parseInt(ler.readLine());
+    
+            for (int i = 0; i < qntDeJogadores; i++){
+                String id = ler.readLine();
+                boolean tipo = Boolean.parseBoolean(ler.readLine());
+                double saldo = Double.parseDouble(ler.readLine());
+                double aposta = Double.parseDouble(ler.readLine());
+                boolean ganhador = Boolean.parseBoolean(ler.readLine());
+                
+                Jogador jogador = new Jogador(id, tipo);
+                jogador.ganhou(saldo);
+                jogador.setAposta(aposta);
+                jogador.setGanhador(ganhador);
+                
+                jogadores[i] = jogador;
+            }
+            
+        }catch (IOException e){ //trata a excecao igual o de cima
+            System.out.println("Erro ao ler os dados do torneio: " + e.getMessage());
+        }
+    }
+
+    private void menuInterface(){
+        System.out.print("(1)incluir_jogador\n"+
+                         "(2)remover_jogador\n"+
+                         "(3)inicializar_torneio\n"+
+                         "(4)placar_do_torneio\n"+
+                         "(5)gravar_dados_do_torneio_em_arquivo\n"+
+                         "(6)ler_os_dados_do_torneio_em_arquivo\n"+
+                         "(7)sair_da_aplicacao\n");
+        
+        int opcao;
+        do{
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            if((opcao < 1) || (opcao > 7))
+                System.out.println("Opcao invalida. Insira um numero entre 1 e 7.");
+        }while((opcao < 1) || (opcao > 7));
+        
+        switch(opcao){
+            case 1:
+                incluirJogador();
+                break;
+            case 2:
+                System.out.println("informe a indentificacao do jogador");
+                String identificacao = scanner.nextLine();
+                removerJogador(identificacao);
+                break;
+            case 3:
+                iniciarTorneio();
+                break;
+            case 4:
+                placarDoTorneio();
+                break;
+            case 5:
+                gravarTorneioArquivo();
+                break;
+            case 6:
+                lerTorneioArquivo();
+                break;
+            case 7:
+                Runtime.getRuntime().exec("cls"); 
+                break;
+            default:
+                System.out.println("Opcao invalida.");
+         }
     }
 }
