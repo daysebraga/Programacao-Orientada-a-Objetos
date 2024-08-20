4c8b959e71d1201a19767bd0f9bd74fb3a7d56bb
Luiz Eduardo Albano Giovanetti <67847441+LuizGiovanetti@users.noreply.github.com>
2024-08-20 16:35:05 -0300

    Update Torneio.java

--- Torneio.java
+++ Torneio.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
-
 
 public class Torneio implements Serializable{
     private Jogador[] jogadores;
